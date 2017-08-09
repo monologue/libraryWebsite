a420f5df58e9a6c4800a2c0f341ed6d123598f1b
monologue <reg.taube@gmail.com>
2017-08-09 13:37:38 +0200

    add second page

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 
+
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/index.html")
-})
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/Werkzeug.html', (req, res) => {
+	res.sendFile(__dirname + '/Werkzeug.html');
+
+});
+
+
 app.listen(1025, () => {
-		console.log("Server listening")
+		console.log("Server listening");
 	})
 

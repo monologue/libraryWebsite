697c8530206693edbdd6b985c9bb4bb26b633c09
monologue <reg.taube@gmail.com>
2017-08-09 14:46:59 +0200

    connect to database

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser=require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+var db
 
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -13,12 +15,22 @@ app.get('/Werkzeug.html', (req, res) => {
 
 });
 
-app.post("/", (req, res) => {
+app.post("/item", (req, res) => {
+	db.collection('item').save(req.body, (err, result) => {
+		if (err) return console.log(err)
+
+		console.log('saved to database')
+		res.redirect('/Werkzeug.html')
+	})
 	//console.log("post")
-	console.log(req.body)
+	//console.log(req.body)
 })
 
-app.listen(1025, () => {
+
+MongoClient.connect('mongodb://localhost:27017/testdatenbank', (err, database) => {
+	if (err) return console.log(err)
+	db = database
+	app.listen(1025, () => {
 		console.log("Server listening");
 	})
-
+})

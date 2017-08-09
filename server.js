0b9d076d66274f00af288f9f9c36805010098245
monologue <reg.taube@gmail.com>
2017-08-09 17:19:33 +0200

    write content to and read from database

--- server.js
+++ server.js
@@ -4,23 +4,31 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient
 var db
 
+app.set('view engine', 'ejs')
+
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/Werkzeug.html', (req, res) => {
-	res.sendFile(__dirname + '/Werkzeug.html');
+app.get('/Werkzeug.ejs', (req, res) => {
+	//res.sendFile(__dirname + '/Werkzeug.html');
+	db.collection('item').find().toArray(function(err, results){
+		if (err) console.log(err)
+	
+		res.render('Werkzeug.ejs', {items: results}) 
+	})
 
 });
+
 
 app.post("/item", (req, res) => {
 	db.collection('item').save(req.body, (err, result) => {
 		if (err) return console.log(err)
 
 		console.log('saved to database')
-		res.redirect('/Werkzeug.html')
+		res.redirect('/Werkzeug.ejs')
 	})
 	//console.log("post")
 	//console.log(req.body)
@@ -30,6 +38,9 @@ app.post("/item", (req, res) => {
 MongoClient.connect('mongodb://localhost:27017/testdatenbank', (err, database) => {
 	if (err) return console.log(err)
 	db = database
+	db.collection('items').find().toArray(function(err, results){
+		console.log(results)
+	})
 	app.listen(1025, () => {
 		console.log("Server listening");
 	})
